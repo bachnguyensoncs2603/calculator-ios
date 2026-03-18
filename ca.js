0c7f3e45fe83b756657a2a68256b3b02c1e51e5c
bachnguyensoncs2603 <bach.nguyen263@hcmut.edu.vn>
2026-03-18 11:23:16 +0700

    fix display after calculated

--- ca.js
+++ ca.js
@@ -1,8 +1,19 @@
 let display = document.getElementById("display");
-
+let calculated = false;
 function append(value) {
     if (display.innerText === "Error") {
         display.innerText = value;
+        calculated = false;
+    }
+      else if (calculated) {
+        if ("+-*/".includes(value)) {
+            // nếu bấm operator → tiếp tục tính
+            display.innerText += value;
+        } else {
+            // nếu bấm số → reset
+            display.innerText = value;
+        }
+        calculated = false;
     }
     else if (display.innerText === "0") {
         if ("0123456789".includes(value)) {
@@ -13,6 +24,7 @@ function append(value) {
         }
 
     } else {
+       
         display.innerText += value;
     }
 }
@@ -25,6 +37,7 @@ function calculate() {
     try {
         console.log(display.innerText);
         display.innerText = eval(display.innerText);
+        calculated = true;
     } catch {
         display.innerText = "Error";
 
